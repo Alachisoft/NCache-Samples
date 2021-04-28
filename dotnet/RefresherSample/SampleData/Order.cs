c6561846679a846c84c35544e2dc17d0c41b7ef9
DIYATECH\muhammad_danyial <muhammad_danyial@diyatech.com>
2021-04-28 10:07:49 +0500

    refresher sample is created for dotnet framework

---
+++ dotnet/RefresherSample/SampleData/Order.cs
@@ -0,0 +1,47 @@
+﻿// ===============================================================================
+// Alachisoft (R) NCache Sample Code
+// ===============================================================================
+// Copyright © Alachisoft.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+// ===============================================================================
+
+using System;
+namespace Alachisoft.NCache.Sample.Data
+{
+    /// <summary>
+    /// Model class for orders
+    /// </summary>
+    [Serializable]
+    public class Order
+    {
+        /// <summary>
+        /// Unique Id of the customer
+        /// </summary>
+        public int OrderID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Unique Id of the customer
+        /// </summary>
+        public string CustomerID
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Unique Id of the customer
+        /// </summary>
+        public string Address
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ dotnet/RefresherSample/SampleData/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alachisoft.NCache.Sample.Data
+{
+    [Serializable]
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Address { get; set; }
+    }
+}
