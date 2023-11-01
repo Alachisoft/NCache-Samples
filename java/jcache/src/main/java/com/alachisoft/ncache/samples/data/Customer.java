a8135daf7f6b5eea24423587ac6a77ad5c7b9a5c
DIYATECH\zain_ul_abideen <zain_ul_abideen@diya-tech.com>
2023-11-01 12:20:23 +0500

    Updated JCache Sample for NCache 5.3.2

--- java/jcache/src/main/java/com/alachisoft/ncache/samples/data/Customer.java
+++ java/jcache/src/main/java/com/alachisoft/ncache/samples/data/Customer.java
@@ -13,56 +13,53 @@ package com.alachisoft.ncache.samples.data;
 
 import java.io.Serializable;
 
-public class Customer implements Serializable 
+public class Customer implements Serializable
 {
+    public Customer(String name, int age, String contactNo, String address) {
+        this.name = name;
+        this.age = age;
+        this.contactNo = contactNo;
+        this.address = address;
+    }
+
     private String name;
     private int age;
     public String contactNo;
     public String address;
-    public String gender;
-    
+
     public Customer() {
-        
+
     }
-    
+
     public void setName(String name){
         this.name = name;
     }
-    
+
     public String getName(){
         return this.name;
     }
-    
+
     public void setAge(int age) {
         this.age = age;
     }
-    
+
     public int getAge() {
         return this.age;
     }
-    
+
     public void setContactNo(String contactNo) {
         this.contactNo = contactNo;
     }
-    
+
     public String getContactNo() {
         return this.contactNo;
     }
-    
+
     public void setAddress(String address) {
         this.address = address;
     }
-    
+
     public String getAddress() {
         return this.address;
     }
-    
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-    
-    public String getGender() {
-        return this.gender;
-    }
-            
 }
