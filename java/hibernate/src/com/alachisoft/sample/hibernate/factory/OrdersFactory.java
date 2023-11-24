1810cff8a151c0b453eed4e493f809f72d7222a5
sania-mughal <115713700+sania-mughal@users.noreply.github.com>
2023-11-24 17:01:23 +0500

    made code compilable and updated the version of hibernate sample.

--- java/hibernate/src/com/alachisoft/sample/hibernate/factory/OrdersFactory.java
+++ java/hibernate/src/com/alachisoft/sample/hibernate/factory/OrdersFactory.java
@@ -66,7 +66,7 @@ public class OrdersFactory
         {
             tx.rollback();
             session.clear();
-            session.disconnect();
+            session.close();
             throw ex;
         }
         return orders;
